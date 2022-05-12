4f93c0e1639b2f988382a75e2ec886497d6d2fc6
EEzim <43553973+EEzim-2333@users.noreply.github.com>
2022-05-12 18:46:35 +0100

    update

--- AsingmentCode.m
+++ AsingmentCode.m
@@ -1,21 +1,22 @@
 %Initialzation Noise and Signal
 clear all
 close all
-load('noise_2021.mat');
-load('OrgSingal.mat');
+load('noise_2021.mat')
+load('OrgSingal.mat')
 newSignal = noise + signal;
+fft_signal = abs(fft(signal));
 fft_newSignal = abs(fft(newSignal));
 plot(fft_newSignal)
 %boundrt indentifer
 length = length(newSignal);
 disp(length)
-slop = 0;
-for counter = 1:2:length
-    slop(length/2) = newSignal(counter-1)-newSignal(counter);
-    %disp(counter)
-    disp(slop(length/2));
-    
-end
-    
+% slop = zeros(1,length,"double");
+% for counter = 1:2:length
+%     slop(counter) = newSignal(counter)-newSignal(counter+1);
+%     disp(counter)
+%     disp(slop(counter));   
+% end
+slop = diff(newSignal);
+abs_slop=abs(slop);
 %Filter Design
 
